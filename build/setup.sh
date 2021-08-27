52bb0c7049df9eaec6434f018a1bf1dcaf6c670d
RXWatcher <jrcole@gmail.com>
2021-08-27 09:16:09 +0200

    revert setup.sh

--- build/setup.sh
+++ build/setup.sh
@@ -13,10 +13,10 @@ add-apt-repository 'deb http://nginx.org/packages/ubuntu/ xenial nginx'
 apt-get -y update
 
 # Install nginx and PHP
-apt-get -y install nginx php$PHP_VERSION-fpm php$PHP_VERSION-mysql php$PHP_VERSION-soap php$PHP_VERSION-common \
+apt-get -y install nginx php$PHP_VERSION-fpm php$PHP_VERSION-mysql php$PHP_VERSION-common \
 	php-imagick php-igbinary php-redis php$PHP_VERSION-bcmath php$PHP_VERSION-opcache \
 	php$PHP_VERSION-enchant php$PHP_VERSION-gd php$PHP_VERSION-imap php$PHP_VERSION-intl \
-	php$PHP_VERSION-json php$PHP_VERSION-xml php$PHP_VERSION-xmlrpc php-pear openssl libssl-dev \
+	php$PHP_VERSION-json php$PHP_VERSION-xml php$PHP_VERSION-xmlrpc php-pear \
     php$PHP_VERSION-zip php$PHP_VERSION-bz2 php$PHP_VERSION-mbstring php$PHP_VERSION-curl \
     htop nano net-tools zip unzip openssh-server
 
